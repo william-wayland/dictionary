e2f59d1946d5673e2a084c0e549e1f714633af17
William Wayland <william.wayland@griffithuni.edu.au>
2017-08-22 06:56:25 +1000

    Revert "Changed add_a_file to add_file"
    This reverts commit 34682718393e3957f7edeb411cc084d66f1a9d38.

--- src/dictionary.cpp
+++ src/dictionary.cpp
@@ -7,9 +7,9 @@ Dictionary::Dictionary():
 
 Dictionary::Dictionary(
   const std::string& path_to_file,
-  Punctuation punct)
+  bool remove_punctuations)
 {
-  add_file(path_to_file, punct);
+  add_a_file(path_to_file, remove_punctuations);
 }
 
 //! Searchs the container for a word
@@ -24,9 +24,9 @@ bool Dictionary::does_contain(const std::string& word)
 }
 
 //! Reads a file from the path, and adds each word found into the container.
-void Dictionary::add_file(
+void Dictionary::add_a_file(
   const std::string& path,
-  Punctuation punct)
+  bool remove_punctuations)
 {
   auto file = std::fstream();
   file.open(path);
@@ -42,7 +42,7 @@ void Dictionary::add_file(
     auto word = std::string();
     if (file >> word)
     {
-      if (punct == Punctuation::Remove) {
+      if (remove_punctuations) {
         word.erase(std::remove(word.begin(), word.end(), '.'), word.end());
         word.erase(std::remove(word.begin(), word.end(), ','), word.end());
         word.erase(std::remove(word.begin(), word.end(), ';'), word.end());
@@ -70,7 +70,7 @@ void Dictionary::add_file(
   has_read_a_file = true;
 }
 
-//! Returns a unmutable reference to the dictionary's container -- safe
+//! Returns a unmutable reference to the dictionary's container
 const std::unordered_set<std::string>& Dictionary::borrow_container() {
   return words;
 }
--- src/dictionary.h
+++ src/dictionary.h
@@ -3,27 +3,20 @@
 #include <unordered_set>
 #include <algorithm>
 
-enum class Punctuation{
-	Remove,
-	Keep
-};
-
 class Dictionary
 {
 public:
 	Dictionary();
 	Dictionary(
     const std::string& path_to_file,
-    Punctuation punct = Punctuation::Remove);
+    bool remove_punctuations = true);
 
-	void add_file(
+	void add_a_file(
     const std::string& path,
-    Punctuation punct = Punctuation::Remove);
+    bool remove_punctuations = true);
 
 	bool does_contain(const std::string& word);
-	const std::unordered_set<std::string>& borrow_container();
-
-
+  const std::unordered_set<std::string>& borrow_container();
 private:
 	std::unordered_set<std::string> words;
 	bool has_read_a_file;
